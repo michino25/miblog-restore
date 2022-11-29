555efdaab7f77ec082710b5f7edb176fab247577
michitio <nguyennhutrung.nnt@gmail.com>
2022-11-29 10:56:14 +0700

    commit

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const path = require('path')
 const express = require('express')
 const morgan = require('morgan')
 const handlebars = require('express-handlebars')
+const route = require('./routes')
 
 const app = express()
 const port = 3000
@@ -18,13 +19,7 @@ app.engine('hbs', handlebars.engine({
 app.set('view engine', 'hbs')
 app.set('views', path.join(__dirname, 'resource/views'))
 
-app.get('/', (req, res) => {
-    res.render('home')
-})
-
-app.get('/news', (req, res) => {
-    res.render('news')
-})
+route(app)
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
