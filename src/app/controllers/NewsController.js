8c2dff4c366c1ccdcc309236ac199e9ee887a2cf
michitio <nguyennhutrung.nnt@gmail.com>
2022-11-29 14:01:43 +0700

    commit

--- src/app/controllers/NewsController.js
+++ src/app/controllers/NewsController.js
@@ -1,14 +1,13 @@
 class NewsController {
-
     // GET /news
     index(req, res) {
-        res.render('news')
+        res.render('news');
     }
 
     // GET /news/:slug
-    show (req, res) {
-        res.send('News Deltail Here')
+    show(req, res) {
+        res.send('News Deltail Here');
     }
 }
 
-module.exports = new NewsController
+module.exports = new NewsController();
--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,14 +1,13 @@
 class SiteController {
-
     // GET /home
     home(req, res) {
-        res.render('home')
+        res.render('home');
     }
 
     // GET /search
-    search (req, res) {
-        res.render('search')
+    search(         req, res) {
+                res.render('search');
     }
 }
 
-module.exports = new SiteController
+module.exports = new SiteController();
